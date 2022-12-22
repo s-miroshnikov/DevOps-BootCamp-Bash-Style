c1d954988012c252402d0cb21f1386589e40c397
Sergei Miroshnikov <s.miroshnikov@outlook.com>
2022-12-23 00:35:31 +0400

    made some fixes

--- game.sh
+++ game.sh
@@ -12,7 +12,8 @@ while [ "${ch}" -ne 3 ]; do
     x=0; c=0; p=0
     read -r -p "Enter any number between 0 and 9 : " n
     while [ $c -eq 0 ]; do
-      x=11; r=$( ( shuf -i 0-9 -n 10 ) )
+      x=11
+      mapfile -t r < <(shuf -i 0-9 -n 10)
       echo "${r[@]}"
       for i in {1..10}; do
         a[i]=$i
@@ -23,7 +24,7 @@ while [ "${ch}" -ne 3 ]; do
         c=1
         break
       fi
-      if [ "${r[$((x))-1]}" -eq "$n" ]; then
+      if [ "${r["$x"-1]}" -eq "${n}" ]; then
         echo "Great"
         p=$(( p + 1 ))
       else
